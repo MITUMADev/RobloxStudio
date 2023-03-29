ad4d8ffd31c389468ca5b8e332e21dd8022d5a22
MITUMADev <87347653+MITUMADev@users.noreply.github.com>
2023-03-29 18:34:19 +0700

    Create InteractingNotifyPolice.lua

---
+++ InteractingNotifyPolice.lua
@@ -0,0 +1,28 @@
+-- Replace "ObjectName" with the name of the object you want to track
+
+local object = game.Workspace:WaitForChild("ObjectName")
+
+-- Replace "PoliceTeam" with the name of the team you want to notify
+
+local policeTeam = game.Teams:FindFirstChild("PoliceTeam")
+
+object.Touched:Connect(function(hit)
+
+    local player = game.Players:GetPlayerFromCharacter(hit.Parent)
+
+    if player and policeTeam then
+
+        local message = "Player " .. player.Name .. " interacted with " .. object.Name
+
+        local notification = Instance.new("Hint", policeTeam.TeamColor)
+
+        notification.Text = message
+
+        wait(5)
+
+        notification:Destroy()
+
+    end
+
+end)
+
